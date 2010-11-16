95a7d09d87236bcc72437b43e0397c95a961b335
John Tantalo <john.tantalo@gmail.com>
2010-11-16 10:10:29 -0800

    test-qunit support for ringo and node

--- test-qunit.js
+++ test-qunit.js
@@ -20,6 +20,49 @@ if (typeof(require) !== 'undefined') {
         local_exports[test] = tests[test]
       }
     };
+  } else {
+    // node, ringo
+    var tests = {};
+      
+    if (typeof(print) === 'undefined') {
+      // node
+      var sys = require('sys');
+      var print = function (s) {
+        sys.print(s+"\n");
+      }
+    }
+    
+    exports.test = function (name, fn) {
+      tests['test '+name] = fn;
+    };
+    exports.ok = function (value, name) {
+      try {
+        assert.ok(value, name);
+      } catch (e) {
+        if (e.name === "AssertionError") {
+          print("    FAIL: "+name);
+        }
+        throw e;
+      }
+    };
+    exports.export_tests = function (local_exports) {
+      print("+ Running");
+      var passes = 0, fails = 0, errors = 0;
+      for (var test in tests) {
+        print("  + Running test "+test);
+        try {
+          tests[test]();
+          passes++;
+        } catch (e) {
+          if (e.name === "AssertionError") {
+            fails++;
+          } else {
+            errors++;
+          }
+        }
+      }
+      print("Passes: "+passes+", Fails: "+fails+", Errors: "+errors)
+    };
   }
 } else if (typeof(load) !== 'undefined') {
   // jsc
