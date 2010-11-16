765276c3ba266bfd177f8f3ab9ee80bf3c3dbe77
John Tantalo <john.tantalo@gmail.com>
2010-11-16 11:51:35 -0800

    added test/extra to harness

--- test/all.js
+++ test/all.js
@@ -2,8 +2,10 @@ if (typeof(require) !== 'undefined') {
   // commonjs
   var Harness = require('../test-harness');
   Harness.run('./test/core');
+  Harness.run('./test/extra');
 } else if (typeof(load) !== 'undefined') {
   // jsc
   var Harness = load('test-harness.js');
   Harness.run('test/core.js');
+  Harness.run('test/extra.js');
 }
